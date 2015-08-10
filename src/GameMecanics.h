1e3f192e0569febbdc8606764cb95c5184b57794
François Laplante <Francois.Laplante.2@ens.etsmtl.ca>
2015-08-10 12:14:08 -0400

    commit du header manquant

--- src/GameMecanics.h
+++ src/GameMecanics.h
@@ -10,7 +10,6 @@ typedef enum { left, right, down } Direction;
 
 void initialiseGameScene(GameState *gameState);
 bool blockIsAt(GameState *gameState, int i, int j);
-void getRndmColor(Color *color);
 bool spawnTetrisShp(GameState *gameState);
 bool moveShape(GameState *gameState, Direction direction);
 void dropShape(GameState *gameState);
